4746a75404c228554255cd22ae648810589c97ce
claudia13-prog <cappellaclaudia13@gmail.com>
2025-03-21 20:13:41 +0100

    update ticket controller

--- src/main/java/org/ticket/platform/ticket_platform/controller/TicketController.java
+++ src/main/java/org/ticket/platform/ticket_platform/controller/TicketController.java
@@ -1,15 +1,28 @@
 package org.ticket.platform.ticket_platform.controller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.ticket.platform.ticket_platform.model.Status;
 import org.ticket.platform.ticket_platform.model.Ticket;
+import org.ticket.platform.ticket_platform.repository.CategoryRepository;
+import org.ticket.platform.ticket_platform.repository.OperatorRepository;
 import org.ticket.platform.ticket_platform.repository.TicketRepository;
+import org.ticket.platform.ticket_platform.service.CategoryService;
+import org.ticket.platform.ticket_platform.service.OperatorService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/tickets")
@@ -18,11 +31,58 @@ public class TicketController {
     @Autowired
     private TicketRepository repository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private OperatorRepository operatorRepository;
+
+    @Autowired
+    private OperatorService operatorService;
+
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping
     public String index(Model model){
         List<Ticket> tickets = repository.findAll();
         model.addAttribute("tickets", tickets);
         return "tickets/index";
     }
-    
+
+    @GetMapping("/search/title")
+    public String findByTitle(@RequestParam(name="title") String title, Model model){
+        List<Ticket> tickets = repository.findByTitleContaining(title);
+        model.addAttribute("tickets", tickets);
+        return "tickets/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model){
+        //Ticket ticket =repository.findById(id).get();
+        model.addAttribute("ticket", repository.findById(id).get());
+        return "tickets/show";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("operators", operatorService.getAllOperators());
+        model.addAttribute("currentDate", LocalDate.now().toString());
+        model.addAttribute("ticket", new Ticket());
+        return "tickets/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("ticket") Ticket formTicket, BindingResult bindingResult, Model model){
+        formTicket.setStatus(Status.APERTO);
+        formTicket.setNotes(new ArrayList<>());
+        
+        if(bindingResult.hasErrors()){
+            return "tickets/create";
+        }
+
+        repository.save(formTicket);
+        return "redirect:/tickets";
+    }
 }
