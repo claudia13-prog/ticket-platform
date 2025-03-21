b1ebcccc8c06e65d2fc8ab3b87facea55f751ac8
claudia13-prog <cappellaclaudia13@gmail.com>
2025-03-21 20:13:20 +0100

    update repository

---
+++ src/main/java/org/ticket/platform/ticket_platform/repository/OperatorRepository.java
@@ -0,0 +1,8 @@
+package org.ticket.platform.ticket_platform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.ticket.platform.ticket_platform.model.Operator;
+
+public interface OperatorRepository extends JpaRepository<Operator, Integer>{
+
+}
--- src/main/java/org/ticket/platform/ticket_platform/repository/TicketRepository.java
+++ src/main/java/org/ticket/platform/ticket_platform/repository/TicketRepository.java
@@ -1,8 +1,11 @@
 package org.ticket.platform.ticket_platform.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ticket.platform.ticket_platform.model.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
     
+    public List<Ticket> findByTitleContaining(String title);
 }
